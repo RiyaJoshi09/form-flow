2b73ee4664494004c7299b15d5c8679948a39fc8
RiyaJoshi09 <riyajoshi040917@gmail.com>
2026-03-22 12:47:10 +0530

    edit field feature added for fields in canvas

---
+++ src/app/pages/edit-field/edit-field.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-field',
+  imports: [],
+  templateUrl: './edit-field.html',
+  styleUrl: './edit-field.css',
+})
+export class EditField {
+
+}
--- src/app/pages/form-builder/form-builder.ts
+++ src/app/pages/form-builder/form-builder.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { generate, identity } from 'rxjs';
+import { EditField } from '../edit-field/edit-field';
 
 @Component({
   selector: 'app-form-builder',
-  imports: [RouterLink, RouterOutlet, MatIconModule, MatCheckboxModule, CommonModule],
+  imports: [RouterLink, RouterOutlet, MatIconModule, MatCheckboxModule, CommonModule, MatDialogModule],
   templateUrl: './form-builder.html',
   styleUrl: './form-builder.css',
 })
 export class FormBuilder {
   formFields: any[] = [];
+  constructor(private dialog: MatDialog) {}
 
   addField(type:string){
     // To add field to canvas
@@ -27,5 +29,22 @@ export class FormBuilder {
   removeField(index:number){
     // To remove field from canvas
     this.formFields.splice(index, 1);
+  }
+
+  editField(index:number): void{
+    // open edit dialog box and edit the copy of it until saved
+    const fieldCopy = JSON.parse(JSON.stringify(this.formFields[index]));
+
+    const dialogRef = this.dialog.open(EditField, {
+      width: '400px',
+      data: fieldCopy
+    } );
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result){
+        //Update field with new data once saved
+        this.formFields[index] = result;
+      }
+    })
   }
 }
