a96d1ccd612bf70bfc40d347972fcaf5d08c072f
suhani-lodhi <suhanilodhi050@gmail.com>
2026-03-23 18:41:08 +0530

    connecting localstorage to dashboard

--- src/app/components/my-forms/my-forms.ts
+++ src/app/components/my-forms/my-forms.ts
@@ -24,7 +24,6 @@ export class MyForms {
   getFormData(){
     let formData=localStorage.getItem('formflow_forms');
     if(formData){
-
       console.log(JSON.parse(formData));
       this.forms=JSON.parse(formData);
     }
@@ -41,6 +40,7 @@ export class MyForms {
 
   deleteForm(id : number){
     this.forms = this.forms.filter(form => form.id !== id);
+    localStorage.setItem('formflow_forms', JSON.stringify(this.forms));
     this.loadSummary();
   }
 }
