58a08e55bece2c1dc9e844a899b9a54deaef7a89
het-naik <141618536+het-naik@users.noreply.github.com>
2026-03-28 18:21:24 +0530

    Added Login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import {MatError, MatFormField, MatInputModule} from '@angular/material/input';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, MatFormField, MatInputModule, MatError],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+
+  email : string = '';
+  password : string = '';
+
+  constructor(private router : Router) {}
+
+  onLogin() {
+
+    if(!this.email || !this.password) {
+      alert('Please enter email and password');
+      return;
+    }
+
+    if (this.password.length < 6) {
+      alert('Password must be at least 6 characters');
+      return;
+    }
+
+    sessionStorage.setItem('isLoggedIn', 'true');
+    this.router.navigate(['/home']);
+  }
 
 }
