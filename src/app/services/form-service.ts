484fef92b3b1250932653b4b6c69338a1b5d32b6
suhani-lodhi <suhanilodhi050@gmail.com>
2026-03-28 10:34:03 +0530

    fixed port issue

--- src/app/services/form-service.ts
+++ src/app/services/form-service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class FormService {
-  url = 'http://localhost:8082/formflow/';
+  url = 'http://localhost:8081/formflow/';
   constructor(private http: HttpClient) {}
 
   getFieldType(type: string) {
