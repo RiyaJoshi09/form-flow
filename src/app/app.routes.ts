2108a9ec10e2f51ad7bb9b12b42401ae3912a750
Pritish <pritishsahoo8338@gmail.com>
2026-03-29 19:50:07 +0530

    Added guard on create route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { Home } from './pages/home/home';
 import { FormBuilder } from './pages/form-builder/form-builder';
 import { NotFound } from './pages/not-found/not-found';
 import { FormSubmission } from './pages/form-submission/form-submission';
+import { Login } from './pages/login/login';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch: "full"},
     {path: "login", component: Login},
     
     {path: "home", component: Home, canActivate: [authGuard]},
-    {path: "create", component: FormBuilder},
+    {path: "create", component: FormBuilder,canActivate: [authGuard]},
 
     {path: "form/:id", component: FormSubmission},
     { path: 'edit-form/:id', component: FormBuilder },
