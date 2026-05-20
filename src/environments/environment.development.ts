7cff2c60962ffdff6d0d7a720dbe764f3d5a43d4
RiyaJoshi09 <riyajoshi040917@gmail.com>
2026-05-20 06:37:43 +0530

    deployment error

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    //backendUrl : 'http://localhost:8080/formflow/'
-    backendUrl : 'https://formflow-twxv.onrender.com/formflow/'
+    backendUrl : 'http://localhost:8080/formflow/'
+    //backendUrl : 'https://formflow-twxv.onrender.com/formflow/'
 };
